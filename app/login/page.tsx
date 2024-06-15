c3401870a88be17efd5341f03bef9856bbeac5af
Yukina <grey.sora.35@gmail.com>
2024-06-16 01:26:35 +0900

    新規登録機能の実装

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,47 @@
+"use client"
+
 import styles from "./styles.module.css";
+import {useState} from "react";
+import {createClient} from "@/utils/supabase/client";
+
 
 export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignUp = async () => {
+        const supabase = createClient();
+
+        const {error} = await supabase.auth.signUp({
+            email,
+            password
+        })
+
+        if(error){
+            console.log(error.message)
+        }
+    }
+
     return (
         <div className={styles.wrap}>
             <div className={styles.loginForm}>
                 <h1 className={styles.title}>ALBUM</h1>
                 <div className={styles.inputWrap}>
                     <label className={styles.label} htmlFor="email">メールアドレス</label>
-                    <input className={styles.input} type="text" name="email" id="email"/>
+                    <input className={styles.input} type="text" name="email" id="email" value={email} onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}/>
                 </div>
 
                 <div className={styles.inputWrap}>
                     <label className={styles.label} htmlFor="password">パスワード</label>
-                    <input className={styles.input} type="password" name="password" id="password"/>
+                    <input className={styles.input} type="password" name="password" id="password" value={password}
+                           onChange={(e) => {
+                               setPassword(e.target.value)
+                           }}/>
                 </div>
 
-                <button className={styles.button}>新規登録する</button>
+                <button className={styles.button} onClick={handleSignUp}>新規登録する</button>
             </div>
         </div>
     )
