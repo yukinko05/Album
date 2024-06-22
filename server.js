8606218af7bf7599b13ef4755ae1dfc7f71a1eea
Yukina <grey.sora.35@gmail.com>
2024-06-22 23:15:15 +0900

    MockAPIのレスポンスが返ってくるまでに１秒かかるように修正

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server")
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// 遅延を追加するミドルウェア
+server.use((req, res, next) => {
+    setTimeout(next, 1000); // 1秒の遅延
+});
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
