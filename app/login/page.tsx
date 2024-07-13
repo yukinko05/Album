9816f4b19283e0bcb42d46d3c4ad634936d53689
Yukina <grey.sora.35@gmail.com>
2024-07-13 18:31:23 +0900

    入力フォームのState管理をReactHookFormに書き換える

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,20 @@
 "use client"
 
 import styles from "./styles.module.css";
-import {useState} from "react";
-import {createClient} from "@/utils/supabase/client";
 import NavigationBar from "@/components/NavigationBar/NavigationBar";
+import {SubmitHandler, useForm} from "react-hook-form";
 
 
+type Inputs = {
+    email: string;
+    password: string;
+}
 export default function LoginPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+    const {register, handleSubmit, formState: {errors},} = useForm<Inputs>()
 
-    const handleSignUp = async () => {
-        setErrorMessage("");
-
-        const supabase = createClient();
-
-        const {error} = await supabase.auth.signUp({
-            email,
-            password
-        })
-
-        if (error) {
-            setErrorMessage(error.message)
-        }
-    }
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        console.log(data);
+    };
 
     return (
         <div>
@@ -33,26 +23,38 @@ export default function LoginPage() {
             <div className={styles.wrap}>
 
                 <div className={styles.loginForm}>
-                    <h1 className={styles.title}>ALBUM</h1>
-                    <div className={styles.inputWrap}>
-                        <label className={styles.label} htmlFor="email">メールアドレス</label>
-                        <input className={styles.input} type="text" name="email" id="email" value={email}
-                               onChange={(e) => {
-                                   setEmail(e.target.value)
-                               }}/>
-                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h1 className={styles.title}>ALBUM</h1>
+                        <div className={styles.inputWrap}>
+                            <label className={styles.label} htmlFor="email">メールアドレス</label>
+                            <input
+                                {...register("email", {
+                                    required: "メールアドレスは必須です。",
+                                })}
+                                className={styles.input}
+                                type="text"
+                            />
+                            {errors.email && (
+                                <span>{errors.email.message}</span>
+                            )}
+                        </div>
 
-                    <div className={styles.inputWrap}>
-                        <label className={styles.label} htmlFor="password">パスワード</label>
-                        <input className={styles.input} type="password" name="password" id="password" value={password}
-                               onChange={(e) => {
-                                   setPassword(e.target.value)
-                               }}/>
-                    </div>
+                        <div className={styles.inputWrap}>
+                            <label className={styles.label} htmlFor="password">パスワード</label>
+                            <input
+                                {...register("password", {
+                                    required: "パスワードを入力してください。"
+                                })}
+                                className={styles.input}
+                                type="password"
+                            />
+                            {errors.password && (
+                                <span>{errors.password.message}</span>
+                            )}
+                        </div>
 
-                    <button className={styles.button} onClick={handleSignUp}>新規登録する</button>
-
-                    {errorMessage ? <p className={styles.errorMessage}>{errorMessage}</p> : null}
+                        <button className={styles.button}>新規登録する</button>
+                    </form>
                 </div>
             </div>
         </div>
