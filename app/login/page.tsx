01c927c6fea4d0e690d9096178ac2f57440e2e21
Yukina <grey.sora.35@gmail.com>
2024-07-13 18:49:21 +0900

    メールアドレスの形式チェックとパスワードの文字数制限を追加

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -21,41 +21,50 @@ export default function LoginPage() {
             <NavigationBar/>
 
             <div className={styles.wrap}>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className={styles.loginForm}
+                >
+                    <h1 className={styles.title}>ALBUM</h1>
+                    <div className={styles.inputWrap}>
+                        <label className={styles.label} htmlFor="email">メールアドレス</label>
+                        <input
+                            {...register("email", {
+                                required: "メールアドレスは必須です。",
+                                pattern: {
+                                    value:
+                                        /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                                    message: "不適切なメールアドレスです。",
+                                },
+                            })}
+                            className={styles.input}
+                            type="text"
+                        />
+                        {errors.email && (
+                            <span className={styles.errorMessage}>{errors.email.message}</span>
+                        )}
+                    </div>
 
-                <div className={styles.loginForm}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <h1 className={styles.title}>ALBUM</h1>
-                        <div className={styles.inputWrap}>
-                            <label className={styles.label} htmlFor="email">メールアドレス</label>
-                            <input
-                                {...register("email", {
-                                    required: "メールアドレスは必須です。",
-                                })}
-                                className={styles.input}
-                                type="text"
-                            />
-                            {errors.email && (
-                                <span>{errors.email.message}</span>
-                            )}
-                        </div>
+                    <div className={styles.inputWrap}>
+                        <label className={styles.label} htmlFor="password">パスワード</label>
+                        <input
+                            {...register("password", {
+                                required: "パスワードを入力してください。",
+                                minLength: {
+                                    value: 6,
+                                    message: "6文字以上入力してください。"
+                                }
+                            })}
+                            className={styles.input}
+                            type="password"
+                        />
+                        {errors.password && (
+                            <span className={styles.errorMessage}>{errors.password.message}</span>
+                        )}
+                    </div>
 
-                        <div className={styles.inputWrap}>
-                            <label className={styles.label} htmlFor="password">パスワード</label>
-                            <input
-                                {...register("password", {
-                                    required: "パスワードを入力してください。"
-                                })}
-                                className={styles.input}
-                                type="password"
-                            />
-                            {errors.password && (
-                                <span>{errors.password.message}</span>
-                            )}
-                        </div>
-
-                        <button className={styles.button}>新規登録する</button>
-                    </form>
-                </div>
+                    <button className={styles.button}>新規登録する</button>
+                </form>
             </div>
         </div>
     )
