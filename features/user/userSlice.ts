ab1e29c37c1a7f7e0ca42d06f73a95b9357b921d
yukinko05 <grey.sora.35@gmail.com>
2025-01-06 02:42:35 +0900

    userSliceのフォーマット

--- features/user/userSlice.ts
+++ features/user/userSlice.ts
@@ -3,21 +3,21 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { User } from "../../types/type";
 
 const initialState: { user: User } = {
-  user: {
-    email: "",
-    uid: "",
-  },
+	user: {
+		email: "",
+		uid: "",
+	},
 };
 
 const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    setData: (state, action: PayloadAction<User>) => {
-      state.user.email = action.payload.email;
-      state.user.uid = action.payload.uid;
-    },
-  },
+	name: "user",
+	initialState,
+	reducers: {
+		setData: (state, action: PayloadAction<User>) => {
+			state.user.email = action.payload.email;
+			state.user.uid = action.payload.uid;
+		},
+	},
 });
 
 export const { setData } = userSlice.actions;
