2245a467a7c0ebaf12abbe66a18e3ac5631f37b8
yukinko05 <grey.sora.35@gmail.com>
2025-01-09 17:55:50 +0900

    supabaseの変数を削除およびNEXT_PUBLIC_FIREBASE_STORAGE_BUKETの値を変更

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "@firebase/auth";
 import { getStorage } from "@firebase/storage";
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
