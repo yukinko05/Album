b02e979758bbd65e37b235e89f89f549b0113d41
yukinko05 <grey.sora.35@gmail.com>
2025-02-04 18:51:32 +0900

    getPhotosを実行してphotoデータを取得し画面に表示する処理を実装

--- app/albums/[id]/page.tsx
+++ app/albums/[id]/page.tsx
@@ -1,115 +1,53 @@
 "use client";
 import NavigationBar from "@/components/NavigationBar";
-// import React, { ChangeEvent, useEffect, useState } from "react";
-// import { Spinner } from "@nextui-org/spinner";
-// import { useDispatch, useSelector } from "react-redux";
-// import { RootState } from "@/store/store";
-// import styles from "./page.module.css";
-// import { setPhotos } from "@/features/photos/photosSlice";
-// import { Button } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "@nextui-org/spinner";
+import styles from "./page.module.css";
+import { getPhotos } from "@/services/photoService";
 
 export default function AlbumPhotosPage({
 	params,
 }: { params: { id: string } }) {
-	// const photos = useSelector((state: RootState) => state.photos.photos);
-	// const dispatch = useDispatch();
-	// const [loading, setLoading] = useState(true);
-	// const [base64Image, setBase64Image] = useState<string | null>(null);
+	const albumId = params.id;
+	const [photos, setPhotos] = useState<string[]>([]);
+	const [loading, setLoading] = useState(true);
 
-	// const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
-	// 	if (e.target.files === null) {
-	// 		return;
-	// 	}
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await getPhotos(albumId);
+				const photoUrls = response.map((photo) => photo.photoUrl);
+				setPhotos(photoUrls);
+				setLoading(false);
+			} catch (error) {
+				console.log("Fetchに失敗しました: ", error);
+				setLoading(false);
+			}
+		};
 
-	// 	const reader = new FileReader();
-
-	// 	reader.onloadend = (evt) => {
-	// 		if (evt.target !== null) {
-	// 			setBase64Image(evt.target.result as string);
-	// 		}
-	// 	};
-
-	// 	reader.readAsDataURL(e.target.files[0]);
-	// };
-
-	// const handleSubmit = async () => {
-	// 	if (base64Image === null) {
-	// 		return;
-	// 	}
-
-	// 	const newPhoto = {
-	// 		url: base64Image,
-	// 		albumId: params.id,
-	// 	};
-
-	// 	try {
-	// 		await fetch("http://localhost:4000/photos", {
-	// 			method: "POST",
-	// 			headers: {
-	// 				"Content-Type": "application/json",
-	// 			},
-	// 			body: JSON.stringify(newPhoto),
-	// 		})
-	// 			.then(() => setBase64Image(null))
-	// 			.catch((error) => console.log(error));
-	// 	} catch (error) {
-	// 		console.log(error)
-	// 	}
-	// }
-
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		try {
-	// 			const response = await fetch(
-	// 				`http://localhost:4000/photos?albumId=${params.id}`,
-	// 			);
-	// 			const data = await response.json();
-	// 			dispatch(setPhotos(data));
-	// 			setLoading(false);
-	// 		} catch (error) {
-	// 			console.log("Fetchに失敗しました: ", error);
-	// 			setLoading(false);
-	// 		}
-	// 	};
-
-	// 	fetchData();
-	// }, []);
+		fetchData();
+	}, []);
 
 	return (
 		<div>
 			<NavigationBar />
-			{/* {loading ? (
+			{loading ? (
 				<div className={styles.loading}>
 					<Spinner />
 				</div>
 			) : (
 				<div className={styles.warp}>
-					{photos.map((photo) => (
+					{photos.map((photo, index) => (
 						<img
-							className={styles.photo}
-							src={photo.url}
-							alt={photo.id}
-							key={photo.id}
+							//TODO:keyはphotoIdを設定
+							key={index}
+							className={styles.cardImg}
+							src={photo}
+							//TODO:alt設定
 						/>
 					))}
 				</div>
 			)}
-			<div className={styles.imgUpload}>
-				<input
-					type="file"
-					id="photo"
-					onChange={handleChangeFile}
-					accept="image/*"
-				/>
-				{base64Image && <img src={base64Image} alt="選択中のアルバム写真" />}
-				<Button
-					color="primary"
-					onClick={handleSubmit}
-					isDisabled={!base64Image}
-				>
-					送信
-				</Button>
-			</div> */}
 		</div>
 	);
 }
