b7958af133de9e0e26c8027782d559d2cc89a885
yukinko05 <grey.sora.35@gmail.com>
2025-02-05 22:53:19 +0900

    エラーハンドリングを改善

--- services/photoService.ts
+++ services/photoService.ts
@@ -32,7 +32,13 @@ export const getPhotos = async (albumId: string) => {
 			})
 			.filter((photo): photo is Photo => photo !== null);
 	} catch (error) {
-		console.error("写真の取得中にエラーが発生しました:", error);
-		throw error;
+		if (error instanceof Error) {
+			console.error(`写真の取得中にエラーが発生しました: ${error.message}`);
+			throw new Error(
+				`アルバム(${albumId})の写真取得に失敗しました: ${error.message}`,
+			);
+		}
+		console.error("予期せぬエラーが発生しました:", error);
+		throw new Error(`予期せぬエラーが発生しました`);
 	}
 };
