f091b2d540ec355653076a458f7b5f37dc677d5e
yukinko05 <grey.sora.35@gmail.com>
2025-02-16 23:20:43 +0900

    シェアID発行ボタンコンポーネントの雛形作成

---
+++ app/albums/albumShareID/ShareIDGenerateButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function ShareIDGenerateButton() {
+  const onGenerateClick = () => { };
+
+  return (
+    <Button onClick={onGenerateClick} color="primary">
+      シェアIDを発行する
+    </Button>
+  );
+}
