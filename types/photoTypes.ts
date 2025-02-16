8872b9f726962090bdf0f99f26ee6ad241985004
yukinko05 <grey.sora.35@gmail.com>
2025-02-16 22:23:36 +0900

    Photosの型を使用している箇所があったので再度追加

--- types/photoTypes.ts
+++ types/photoTypes.ts
@@ -1,18 +1,23 @@
 export interface Photo {
-	photoId: string;
-	createdAt: string;
-	albumId: string;
-	photoUrl: string;
-	userId: string;
+  photoId: string;
+  createdAt: string;
+  albumId: string;
+  photoUrl: string;
+  userId: string;
 }
 
+export interface Photos {
+  id: string;
+  url: string;
+  albumId: string;
+}
 export interface PhotosProps {
-	albumId: string;
-	photos: Photo[];
+  albumId: string;
+  photos: Photo[];
 }
 
 export interface AddPhotosRequest {
-	albumId: string;
-	photosList: string[];
-	uid: string;
+  albumId: string;
+  photosList: string[];
+  uid: string;
 }
