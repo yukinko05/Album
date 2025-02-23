9aa8e6bc9eacfaf08e51658ef0e1b67b37d2cac2
yukinko05 <grey.sora.35@gmail.com>
2025-02-23 16:36:31 +0900

    サイドバーをコンポーネントの雛形作成

---
+++ components/SideBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import ShareRoomSidebarList from "@/components/ShareRoom/ShareRoomSidebarList";
+
+export default function SideBar() {
+  return (
+    <>
+      <ShareRoomSidebarList />
+      {/* TODO:ルーム作成ボタン */}
+      {/* TODO：ユーザー情報 */}
+      {/* TODO:ログアウトボタン */}
+    </>
+  );
+}
