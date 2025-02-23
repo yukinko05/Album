913242ac33e0a3b68642afe08e07de053343951a
yukinko05 <grey.sora.35@gmail.com>
2025-02-24 05:52:14 +0900

    シェアルーム機能の追加に伴いalbumRepository,albumService,albumTypesをリファクタリング

--- types/albumTypes.ts
+++ types/albumTypes.ts
@@ -1,58 +1,48 @@
-import { Timestamp } from "firebase/firestore";
-
 export interface Album {
-	albumId: string;
-	title: string;
-	createdAt: string;
-	coverPhotoUrl: string;
-	userId: string;
-	sharedWith: Array<string>;
+  albumId: string;
+  title: string;
+  createdAt: string;
+  coverPhotoUrl: string;
+  userId: string;
+  shareRoomId: string;
 }
 
 export interface AlbumUpdateRequest {
-	data: {
-		title: string;
-		coverPhotoUrl: string;
-	};
-	id: string;
+  data: {
+    title: string;
+    coverPhotoUrl: string;
+  };
+  albumId: string;
 }
 
 export interface EditAlbumTitleRequest {
-	title: string;
-	albumId: string;
+  title: string;
+  albumId: string;
 }
 
 export interface EditAlbumCoverPhotoRequest {
-	coverPhotoUrl: string;
-	albumId: string;
+  coverPhotoUrl: string;
+  albumId: string;
 }
 
-export interface CreateAlbumRequest {
-	id: string | null;
-	title: string;
-	coverPhotoUrl: string | null;
-	createdAt: Timestamp;
-	photos: string[];
-	uid: string;
-	sharedWith: Array<string>;
-}
 export interface AlbumCreateInputs {
-	albumData: {
-		title: string;
-		photos: string[];
-	};
-	uid: string;
+  albumData: {
+    title: string;
+    photos: string[];
+  };
+  userId: string;
+  shareRoomId: string;
 }
 
 export interface AlbumFormProps {
-	title: string;
-	file: FileList[];
+  title: string;
+  file: FileList[];
 }
 
 export interface DeleteAlbumRequest {
-	albumId: string;
-	photos: {
-		photoId: string;
-		photoUrl: string;
-	}[];
+  albumId: string;
+  photos: {
+    photoId: string;
+    photoUrl: string;
+  }[];
 }
