68f3fdfb93bf090a12dc2417b2eda6b3d5a900fe
yukinko05 <grey.sora.35@gmail.com>
2025-02-25 04:41:12 +0900

    ログアウトコンポーネント追加

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,8 +2,20 @@
 
 import ShareRoomSidebarList from "@/components/ShareRoom/ShareRoomSidebarList";
 import Link from "next/link";
+import { useContext } from "react";
+import { authContext } from "@/features/auth/AuthProvider";
+import { useRouter } from "next/navigation";
+import SignOut from "@/app/signout/signout";
 
 export default function SideBar() {
+	const { currentUser } = useContext(authContext);
+	const userId = currentUser?.uid;
+	const router = useRouter();
+
+	if (!userId) {
+		router.push("/login");
+	}
+
 	return (
 		<>
 			<ShareRoomSidebarList />
@@ -15,7 +27,7 @@ export default function SideBar() {
 				ルーム作成
 			</Link>
 			{/* TODO：ユーザー情報 */}
-			{/* TODO:ログアウトボタン */}
+			{userId && <SignOut />}
 		</>
 	);
 }
