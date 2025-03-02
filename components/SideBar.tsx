d77838bf893ed6fcee9df1ddb7ef6d61edde59fd
yukinko05 <grey.sora.35@gmail.com>
2025-03-02 15:48:32 +0900

    Sidebarコンポーネントでユーザーデータを取得する処理を実装

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,20 +2,44 @@
 
 import ShareRoomSidebarList from "@/components/ShareRoom/ShareRoomSidebarList";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { authContext } from "@/features/auth/AuthProvider";
 import { useRouter } from "next/navigation";
 import SignOut from "@/app/signout/signout";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "@/store/store";
+import { getUser } from "@/services/userService";
 
 export default function SideBar() {
 	const { currentUser } = useContext(authContext);
 	const userId = currentUser?.uid;
 	const router = useRouter();
+	const dispatch = useDispatch<AppDispatch>();
+	const [userName, setUserName] = useState<string>("");
+	const [iconImg, setIconImg] = useState<string | null>(null);
 
 	if (!userId) {
 		router.push("/login");
 	}
 
+	useEffect(() => {
+		if (!userId) return;
+
+		const fetchUserData = async () => {
+			try {
+				const response = await dispatch(getUser(userId)).unwrap();
+
+				setUserName(response.userName);
+				setIconImg(response.iconImg);
+			} catch (error) {
+				console.log("ユーザーデータの取得に失敗しました: ", error);
+			}
+		};
+
+		fetchUserData();
+	}, [userId, dispatch]);
+
+	console.log(iconImg);
 	return (
 		<>
 			<ShareRoomSidebarList />
