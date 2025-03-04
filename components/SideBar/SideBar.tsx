e404466bb1163241b8cdbb2261a3845f20d178d0
yukinko05 <grey.sora.35@gmail.com>
2025-03-05 07:48:13 +0900

    ルーム参加リンクにaria属性を追加

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -63,6 +63,7 @@ export default function SideBar() {
 							pathname: "/shareRoomJoinForm",
 						}}
 						className="block rounded-lg px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-blue-500 transition-colors"
+						aria-label="既存の共有ルームに参加"
 					>
 						ルーム参加
 					</Link>
