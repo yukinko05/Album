5610a5e14a8973dbee87bdd8382090b0892a5c5a
yukinko05 <grey.sora.35@gmail.com>
2025-03-08 23:04:16 +0900

    使用していない型を削除

--- types/photoTypes.ts
+++ types/photoTypes.ts
@@ -1,23 +1,16 @@
 export interface Photo {
-	photoId: string;
-	createdAt: string;
-	albumId: string;
-	photoUrl: string;
-	userId: string;
-}
-
-export interface Photos {
-	photoId: string;
-	url: string;
-	albumId: string;
+  photoId: string;
+  createdAt: string;
+  albumId: string;
+  photoUrl: string;
+  userId: string;
 }
 export interface PhotosProps {
-	albumId: string;
-	photos: Photo[];
+  albumId: string;
+  photos: Photo[];
 }
-
 export interface AddPhotosRequest {
-	albumId: string;
-	photosList: string[];
-	userId: string;
+  albumId: string;
+  photosList: string[];
+  userId: string;
 }
